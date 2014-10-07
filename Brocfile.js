11720184913d4292f3dc694d8e47bdd27f882b10
Dean Marano <dmarano@groupon.com>
2014-10-06 21:02:26 -0500

    Look #1.

--- Brocfile.js
+++ Brocfile.js
@@ -7,6 +7,7 @@ var app = new EmberApp();
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
+app.import('bower_components/FitText.js/jquery.fittext.js');
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
 // object's keys should be the environment name and the values
